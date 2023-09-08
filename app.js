d7acd4d0a391e9de81b4f91e57d3839291f9c98c
ga-vance <gregory.vance@alumni.ucalgary.ca>
2023-09-08 11:25:28 -0600

    Add express session and configure cookie

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError");
 const path = require("path");
 const methodOverride = require("method-override");
+const session = require("express-session");
+
 const campgrounds = require("./routes/camprounds");
 const reviews = require("./routes/reviews");
 
@@ -24,6 +26,18 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "public")));
+
+const sessionConfig = {
+    secret: "thisshouldbeabettersecret!",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7,
+    },
+};
+app.use(session(sessionConfig));
 
 app.use("/campgrounds", campgrounds);
 app.use("/campgrounds/:id/reviews", reviews);
