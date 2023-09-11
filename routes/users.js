dbdd9947926959f37de19af90f7627d9c90b1a43
ga-vance <71193788+ga-vance@users.noreply.github.com>
2023-09-11 13:32:26 -0600

    Add redirect to logged in on register

--- routes/users.js
+++ routes/users.js
@@ -23,13 +23,18 @@ router.get("/register", (req, res) => {
 router.post(
     "/register",
     validateUser,
-    catchAsync(async (req, res) => {
+    catchAsync(async (req, res, next) => {
         try {
             const { email, username, password } = req.body.user;
             const user = new User({ email, username });
             const registeredUser = await User.register(user, password);
-            req.flash("success", "Welcome to Yelp Camp!");
-            res.redirect("/campgrounds");
+            req.login(registeredUser, (err) => {
+                if (err) {
+                    return next(err);
+                }
+                req.flash("success", "Welcome to Yelp Camp!");
+                res.redirect("/campgrounds");
+            });
         } catch (e) {
             req.flash("error", e.message);
             res.redirect("/register");
