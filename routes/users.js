fc2bde383d04d372e636ecdd60584feb8393af8a
ga-vance <71193788+ga-vance@users.noreply.github.com>
2023-09-11 11:48:45 -0600

    Add login, and protect some routes so user must be logged in

--- routes/users.js
+++ routes/users.js
@@ -53,4 +53,14 @@ router.post(
     }
 );
 
+router.get("/logout", (req, res) => {
+    req.logout(function (err) {
+        if (err) {
+            return next(err);
+        }
+        req.flash("success", "Goodbye!");
+        res.redirect("/campgrounds");
+    });
+});
+
 module.exports = router;
