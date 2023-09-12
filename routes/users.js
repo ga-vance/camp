8f8ba217c7d8d3a73b57b2915fed4c6fa9ff3822
ga-vance <71193788+ga-vance@users.noreply.github.com>
2023-09-12 11:06:04 -0600

    Condense routes

--- routes/users.js
+++ routes/users.js
@@ -6,21 +6,22 @@ const catchAsync = require("../utils/catchAsync");
 const passport = require("passport");
 const { storeReturnTo, validateUser } = require("../middleware");
 
-router.get("/register", users.renderRegister);
+router
+    .route("/register")
+    .get(users.renderRegister)
+    .post(validateUser, catchAsync(users.register));
 
-router.post("/register", validateUser, catchAsync(users.register));
-
-router.get("/login", users.renderLogin);
-
-router.post(
-    "/login",
-    storeReturnTo,
-    passport.authenticate("local", {
-        failureFlash: true,
-        failureRedirect: "/login",
-    }),
-    users.login
-);
+router
+    .route("/login")
+    .get(users.renderLogin)
+    .post(
+        storeReturnTo,
+        passport.authenticate("local", {
+            failureFlash: true,
+            failureRedirect: "/login",
+        }),
+        users.login
+    );
 
 router.get("/logout", users.logout);
 
