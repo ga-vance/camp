8730af11dc16de4a27f7892a44e638059bad1815
ga-vance <71193788+ga-vance@users.noreply.github.com>
2023-09-18 14:50:00 -0600

    Add cluster map to index page

--- models/campground.js
+++ models/campground.js
@@ -10,39 +10,54 @@ ImageSchema.virtual("thumbnail").get(function () {
     return this.url.replace("/upload", "/upload/w_200");
 });
 
-const CampgroundSchema = new Schema({
-    title: String,
-    images: [ImageSchema],
-    geometry: {
-        type: {
-            type: String,
-            enum: ["Point"],
-            required: true,
+const opts = { toJSON: { virtuals: true } };
+
+const CampgroundSchema = new Schema(
+    {
+        title: String,
+        images: [ImageSchema],
+        geometry: {
+            type: {
+                type: String,
+                enum: ["Point"],
+                required: true,
+            },
+            coordinates: {
+                type: [Number],
+                required: true,
+            },
         },
-        coordinates: {
-            type: [Number],
-            required: true,
+        price: Number,
+        description: String,
+        location: String,
+        author: {
+            type: Schema.Types.ObjectId,
+            ref: "User",
         },
+        reviews: [
+            {
+                type: Schema.Types.ObjectID,
+                ref: "Review",
+            },
+        ],
     },
-    price: Number,
-    description: String,
-    location: String,
-    author: {
-        type: Schema.Types.ObjectId,
-        ref: "User",
-    },
-    reviews: [
-        {
-            type: Schema.Types.ObjectID,
-            ref: "Review",
-        },
-    ],
-});
+    opts
+);
 
 CampgroundSchema.post("findOneAndDelete", async function (doc) {
     if (doc) {
         await Review.deleteMany({ _id: { $in: doc.reviews } });
     }
+});
+
+CampgroundSchema.virtual("properties.popUpMarkup").get(function () {
+    return `
+    <strong>
+        <a href="/campgrounds/${this._id}">${this.title}</a>
+    </strong>
+    <p>
+    ${this.location}
+    </p>`;
 });
 
 module.exports = mongoose.model("Campground", CampgroundSchema);
