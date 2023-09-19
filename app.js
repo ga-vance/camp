5cb2921885479a3d21da850f63901419254a0b15
ga-vance <71193788+ga-vance@users.noreply.github.com>
2023-09-19 13:07:23 -0600

    Add mongo injection sanitization

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const session = require("express-session");
 const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
+const mongoSanitize = require("express-mongo-sanitize");
 
 const User = require("./models/user");
 const userRoutes = require("./routes/users");
@@ -35,6 +36,7 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(mongoSanitize());
 
 const sessionConfig = {
     secret: "thisshouldbeabettersecret!",
